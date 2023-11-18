e472a3ade5b091b2efac811a4591b202a3e148ff
dharmikdashlani <dharmikdashlani@gmail.com>
2023-11-18 14:35:09 +0530

    app creared

--- lib/DB/signupDB.dart
+++ lib/DB/signupDB.dart
@@ -7,13 +7,13 @@ class RegisterDB {
     Map<String, String> h = {
       'Content-Type': 'application/x-www-form-urlencoded'
     };
-    print("dharmik");
+    // print("dharmik");
     http.Response response = await http.post(
         Uri.parse(
             "https://unwound-human.000webhostapp.com/PHP/Api/add_customer_api.php"),
         body: data,
         headers: h);
-    print("dharmik");
+    // print("dharmik");
     if (response.statusCode == 200) {
       List data1 = await jsonDecode(response.body);
       print(data1);
